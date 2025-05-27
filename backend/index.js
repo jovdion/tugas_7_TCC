02b66d77d1aa3500e43e98f145a0ffa3560200a9
jovano dion manuel <jovanodionm@gmail.com>
2025-05-27 14:23:33 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,8 +24,8 @@ const projectRoot = path.join(__dirname, "..");
 // Define allowed origins
 const allowedOrigins = [
   'https://tugas-7-dion-dot-g-01-02.uc.r.appspot.com',  // Your frontend URL
-  'http://localhost:3000',
-  'https://tugas-7-dion-913201672104.us-central1.run.app',  // Local development
+  'http://localhost:3000',  // Local development
+   'https://tugas-7-dion-913201672104.us-central1.run.app',
 ];
 
 app.use(cors({
